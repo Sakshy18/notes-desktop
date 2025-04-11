5dc7767833ef9766e3cddb2538981dea0914ba47
sakshi <isakshy18@gmail.com>
2025-04-11 21:06:07 +0530

    fix: removed electron reload from prod

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
-require('electron-reload')(__dirname, {
-    electron: require(`${__dirname}/node_modules/electron`)
-  });
-  
-  const { app, BrowserWindow, ipcMain } = require('electron');
-  const path = require('path');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
-  let win ;
-  function createWindow () {
-      win = new BrowserWindow({
-      frame:false,
-      movable:true,
-      width: 400,
-      height: 550,
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-      }
-    });
-  
-    win.loadFile('index.html');
+let win;
 
-
-  }
-  
-  app.whenReady().then(createWindow);
-
-  ipcMain.on('minimize-window', () => {
-    win.minimize();
+function createWindow () {
+  win = new BrowserWindow({
+    frame: false,
+    movable: true,
+    width: 400,
+    height: 550,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
   });
 
-  ipcMain.on('close-window', () => {
-    win.close();
-  });
-  
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+ipcMain.on('minimize-window', () => {
+  win.minimize();
+});
+
+ipcMain.on('close-window', () => {
+  win.close();
+});
